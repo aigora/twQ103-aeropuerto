1a86f058e4a15e847fe865f14257a02bca741f73
IkertxoGM <47816824+IkertxoGM@users.noreply.github.com>
2019-04-10 12:39:44 +0200

    Update tabajo.c

--- tabajo.c
+++ tabajo.c
@@ -1,14 +1,14 @@
 /*Programa que registra los tiempos de 10 aviones en un aeropuerto, cuyos numeros de vuelo van del 1 al 10.
-El menú principal del programa proporciona las siguientes opciones:
+El menÃº principal del programa proporciona las siguientes opciones:
 
 1.	Registrar tiempo de un avion.
 2.	Lista de resultados
-3.	Clasificación de los aterrizajes y despegues
+3.	ClasificaciÃ³n de los aterrizajes y despegues
 4.	Tiempo medio de aterrizaje y/o despegue
 
-La opción 1 debe registrar el tiempo de un avion en horas minutos y segundos, indicando su numero de vuelo.
-La opción 2 muestra la lista de mejor a peor tiempo.
-La opción 4 indica la media de tiempos de todos los aviones que han finalizado el despegue y/o el aterrizaje
+La opciÃ³n 1 debe registrar el tiempo de un avion en horas minutos y segundos, indicando su numero de vuelo.
+La opciÃ³n 2 muestra la lista de mejor a peor tiempo.
+La opciÃ³n 4 indica la media de tiempos de todos los aviones que han finalizado el despegue y/o el aterrizaje
 
 */
 
@@ -18,7 +18,7 @@ La opción 4 indica la media de tiempos de todos los aviones que han finalizado e
 #include <string.h>
 
 #define OK 0//Defino los valores a devolver por cada una de las funciones
-#define ERROR -1//Uso OK y ERROR en lugar de 0 ó 1 pues es mucho más visual
+#define ERROR -1//Uso OK y ERROR en lugar de 0 Ã³ 1 pues es mucho mÃ¡s visual
 
 
 
@@ -26,14 +26,19 @@ La opción 4 indica la media de tiempos de todos los aviones que han finalizado e
 typedef enum//Defino el tipo boolean porque necesito variables y funciones
  {
         FALSE = 0,//que devuelvan un valor que toman solo 2 opciones (0 y 1 normalmente en C)
-        TRUE = 1//pero en todos los códigos y lenguajes está la opción de un tipo bool
+        TRUE = 1//pero en todos los cÃ³digos y lenguajes estÃ¡ la opciÃ³n de un tipo bool
 } bool;//que devuelve true y false
 
+typedef struct{
+	int horas;
+	int minutos;
+	int segundos;
+	int toltal;
+}Tiempo;
 
 
 
-
-/*declaración de funciones*/
+/*declaraciÃ³n de funciones*/
 
 int pintaMenu();
 int main ()
@@ -41,15 +46,15 @@ int main ()
     
 
 
-int leido=-1; //Variable para el menú
+int leido=-1; //Variable para el menÃº
 
 
 	while (leido!=5)//Mientras no seleccione el 5(salir)
 	{
- 		leido=pintaMenu();//Sale el menú.
-			switch(leido)//Analizo la opción
+ 		leido=pintaMenu();//Sale el menÃº.
+			switch(leido)//Analizo la opciÃ³n
 		{
-           case ERROR://Error en la inseción de datos
+           case ERROR://Error en la inseciÃ³n de datos
            printf ("Ha habido un problema.\n");
            system ("pause");
            break;   
@@ -57,11 +62,11 @@ int leido=-1; //Variable para el menú
            				break;
 			case 2://Si se marca 2: se lista panel de estado de vuelos
            				break;
-			case 3://Si se marca 3: se muestra la clasificación
+			case 3://Si se marca 3: se muestra la clasificaciÃ³n
             				break;	
 			case 4://Si se marca 4: se saca la media
     				break;
-			default://Cualquier otra opción: no vale
+			default://Cualquier otra opciÃ³n: no vale
 				break;
 		}
 	}
@@ -70,7 +75,7 @@ int leido=-1; //Variable para el menú
 }
 	
 	
-	int pintaMenu()//Función que muestra el menú y captura la opción del usuario
+	int pintaMenu()//FunciÃ³n que muestra el menÃº y captura la opciÃ³n del usuario
 {
 	int option=0,cde;
     system("cls");//Para limpiar la pantalla 
@@ -85,7 +90,7 @@ int leido=-1; //Variable para el menú
 	printf("---------------------------------------\n");
 	printf("\nIndique su opcion: ");
 
-	cde=scanf("%d", &option);//Se saca el cursor para que el usuario pueda meter una opción
+	cde=scanf("%d", &option);//Se saca el cursor para que el usuario pueda meter una opciÃ³n
 	
 
 	if (cde==0)//Control de errores
